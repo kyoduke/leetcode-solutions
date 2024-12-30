5f0906e3df6569e00d2a344f3fe88448b84df62e
Caio Duque <caioduque.dev@gmail.com>
2024-12-30 19:45:07 -0300

    added solution for problem 2: Valid Parentheses

---
+++ solutions/0020-valid-parentheses.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+def is_valid(text: str) -> bool:
+    pairs = {"(": ")", "[": "]", "{": "}"}
+    stack = []
+    for char in text:
+        if char in pairs:
+            stack.append(char)
+        elif not stack:
+            return False
+
+        else:
+            top = stack[-1]
+            if char != pairs[top]:
+                return False
+            stack.pop()
+
+    return True if len(stack) == 0 else False
+
+
+class TestValidParentheses(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(True, is_valid("[](){}"))
+
+    def test_2(self):
+        self.assertEqual(False, is_valid("[(){}"))
+
+    def test_3(self):
+        self.assertEqual(False, is_valid("[]){}"))
+
+    def test_4(self):
+        self.assertEqual(True, is_valid("({}[][[]]()([()]))"))
+
+    def test_5(self):
+        self.assertEqual(False, is_valid("({}[][[]]()([()])))"))
